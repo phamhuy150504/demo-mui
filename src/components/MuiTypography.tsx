77f0ff3bd58afacdd079b96699bb055818ade4ed
phamhuy <huy.pg@sanbul.vn>
2023-05-29 17:52:36 +0700

    comback home

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,20 @@
+import { Typography } from '@mui/material'
+
+export const MuiTypography = () => {
+  return (
+    <div>
+        <Typography variant='h1'>H1 Heading</Typography>
+        <Typography variant='h2'>H2 Heading</Typography>
+        <Typography variant='h3'>H3 Heading</Typography>
+        <Typography variant='h4'>H4 Heading</Typography>
+        <Typography variant='h5' gutterBottom>H5 Heading</Typography>
+        <Typography variant='h6'>H6 Heading</Typography>
+
+        <Typography variant='subtitle1'>SubTitle 1</Typography>
+        <Typography variant='subtitle2'>SubTitle 2</Typography>
+
+        <Typography variant='body1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, numquam.</Typography>
+        <Typography variant='body2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, optio?</Typography>
+    </div>
+  )
+}
